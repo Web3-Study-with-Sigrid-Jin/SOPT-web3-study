66ff41932893803aafc89698e85f6a97233f87bd
Jin Hyung Park <sigrid.jinhyung@gmail.com>
2022-04-11 13:39:00 +0900

    example

---
+++ week1/contracts/MyToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20("Test", "TET") payable {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
