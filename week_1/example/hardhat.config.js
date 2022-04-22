59f8bf33873d7ff9b01a27e2b1f5402953840a68
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-04-22 17:11:30 +0900

    feat: week1

--- week_1/example/hardhat.config.js
+++ week_1/example/hardhat.config.js
@@ -17,11 +17,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
   networks: {
-    ropsten: {
-      url: "https://ethereum-ropsten-rpc.allthatnode.com/",
-      accounts: [""]
-    }
+    // ropsten: {
+    //   url: "https://ethereum-ropsten-rpc.allthatnode.com/",
+    //   accounts: [""]
+    // }
   }
 };
