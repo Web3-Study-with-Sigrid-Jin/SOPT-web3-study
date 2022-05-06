996f7dbdcf2204f926cf31e5be687dacabaf73d9
Jin Hyung Park "Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-06 16:35:15 +0900

    refactor: add configuration

--- week_4/NFTExample/hardhat.config.js
+++ week_4/NFTExample/hardhat.config.js
@@ -24,17 +24,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // gasReporter: {
+  //   enabled: process.env.REPORT_GAS !== undefined,
+  //   currency: "USD",
+  // },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
