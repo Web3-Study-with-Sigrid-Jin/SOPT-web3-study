47d1c0f7f10df48869db62e9fabce89228bb4685
Jin Hyung Park | Sigrid Jin <sigrid.jinhyung@gmail.com>
2022-05-06 16:47:42 +0900

    Update App.js

--- week_4/NFTExample/frontend/src/App.js
+++ week_4/NFTExample/frontend/src/App.js
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
 import myEpicNft from './utils/MyEpicNFT.json';
 
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'DSRV';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
